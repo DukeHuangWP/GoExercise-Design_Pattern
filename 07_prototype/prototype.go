08030c71871d083097670eb7f202d793152cc345
duke <duke.huang.wp@gmail.com>
2021-05-06 23:40:20 +0800

    修正原型模式

--- 07_prototype/prototype.go
+++ 07_prototype/prototype.go
@@ -2,41 +2,55 @@ package prototype
 
 import "fmt"
 
-//蛇的專屬builder
-type SnakeBuilder interface {
-	Set(name string, length int) SnakeBuilder
-	Clone() (*Snake, error)
+//動物的原型接口,負責載入範本與複製動物
+type AnimalPrototype interface {
+	Set(name string, weight int) AnimalPrototype
+	Clone() *Animal
 }
 
-//每個蛇都有名子跟長度
-type Snake struct {
-	ProtoName string
-	Level     int
-
-	Name   string
-	Length int
+//動物的共同屬性
+type Animal struct {
+	ProtoName string //原型範本名稱(方便追蹤範本來源)
+	Level     int //該動物的等級
+	Name   string //該動物的名字
+	Weight int //該動物的重量
 }
 
-//每個蛇都可以取名子
-func (this *Snake) Set(name string, length int) SnakeBuilder {
+//每個複製動物都可以再取名字跟設定重量
+func (this *Animal) Set(name string, weight int) AnimalPrototype {
 	this.Name = name
-	this.Length = length
+	this.Weight = weight
 	return this
 }
 
-//複製一條蛇的範本
-func (this *Snake) Clone() (*Snake, error) {
+//複製該動物
+func (this *Animal) Clone() *Animal {
 	protoName := this.ProtoName
 	if protoName == "" {
-		return nil, fmt.Errorf("原型名稱不可為空")
+		return nil
 	}
 
-	protoName += "_clone"
-	return &Snake{Name: protoName, ProtoName: protoName, Level: this.Level}, nil
+	return &Animal{Name: protoName+"_clone", ProtoName: protoName, Level: this.Level}
+}
+
+//蛇是動物的一種
+type Snake struct {
+	Animal
 }
 
 //此條蛇開始爬行
 func (this *Snake) Scramble() {
-	fmt.Printf("I'm scrambling, my name is %v, length is %v ,level is %v \n", this.Name, this.Length, this.Level)
+	fmt.Printf("I'm scrambling, my name is %v (prototype:%v), weight is %v ,level is %v \n", this.Name, this.ProtoName, this.Weight, this.Level)
+	return
+}
+
+//青蛙是動物的一種
+type Frog struct {
+	Animal
+}
+
+//此青蛙開始游泳
+func (this *Frog) Swimming() {
+	fmt.Printf("I'm swimming, my name is %v (prototype:%v), weight is %v ,level is %v \n", this.Name, this.ProtoName, this.Weight, this.Level)
 	return
 }
--- 07_prototype/prototype_test.go
+++ 07_prototype/prototype_test.go
@@ -1,28 +1,24 @@
 package prototype
 
 import (
-	"fmt"
 	"testing"
 )
 
-//** 建造者模式說明 **
-//透過Director(struct)透過接口選擇子Builder設定屬性,並將物件輸出
-//優點:結構分工細膩可自由將建造過程細部拆解或是一次性輸出
-//缺點:Director分類構造較為繁瑣
+//** 原型模式說明 **
+//可以透過接口載入原型範本,並且所輸出的struct具備複製(clone)功能
+//優點:因輸出的struct具備複製(clone)功能,非常容易生產規格近似的struct
+//缺點:複製前clone需要規劃餅設定好原型範本
 func TestPrototype(t *testing.T) {
 
-	temple := &Snake{ProtoName: "模範蛇", Level: 99}
-	snake1,_ := temple.Clone()
-	snake1.Set("蛇一號",100)
-	fmt.Println(snake1.ProtoName)
+	frog := &Frog{Animal: Animal{ProtoName: "模範青蛙", Level: 11, Name: "青蛙一號"}}
+	frog.Swimming() //I'm swimming, my name is 青蛙一號 (prototype:模範青蛙), weight is 0 ,level is 11
 
-	snake2,_ := snake1.Clone()
-	fmt.Println(snake2.ProtoName)
+	snakeTemple := Animal{ProtoName: "模範蛇", Level: 99}
+	snake1 := &Snake{Animal: snakeTemple}
+	snake1.Set("蛇一號", 100)
+	snake2 := &Snake{Animal: *snake1.Clone()}
 
-	snake1.Scramble()
-	snake2.Scramble()
-	// 範蛇_clone
-	// 模範蛇_clone_clone
-	// I'm scrambling, my name is 蛇一號, length is 100 ,level is 99 
-	// I'm scrambling, my name is 模範蛇_clone_clone, length is 0 ,level is 99 
+	snake1.Scramble() //I'm scrambling, my name is 蛇一號 (prototype:模範蛇), weight is 100 ,level is 99
+	snake2.Scramble() //I'm scrambling, my name is 模範蛇_clone (prototype:模範蛇), weight is 0 ,level is 99
+
 }
